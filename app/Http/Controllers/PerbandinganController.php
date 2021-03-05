dcf04fbc5385becac28f4b612690e58ce46e19c1
Zahrul <zahrulzizki09@gmail.com>
2021-03-05 07:59:33 +0700

    tambahan

--- app/Http/Controllers/PerbandinganController.php
+++ app/Http/Controllers/PerbandinganController.php
@@ -12,20 +12,22 @@ class PerbandinganController extends Controller
 
     public function index(Request $request)
     {
-
+        $kabupaten =Tetangga::select('kabupaten','id')->get();
         
         $data = Tetangga::query();
         if($request->tetangga != null){
-            $data = $data->where('id','=',$request->tetangga)->get();
+            $data = $data->where('id','=',$request->tetangga)->first();
         }
         else{
             
             $data = $data->where('id','=',2)->first();
         // $data = $data->first();
         };
-
+        // return $data->tetangga; 
         $tetangga = explode(",", $data->tetangga);
+        // return 'siini';
+        // return $tetangga;
         // return count($tetangga);
-        return view('perbandingan',['tetangga'=>$tetangga,'data'=>$data]);
+        return view('perbandingan',['tetangga'=>$tetangga,'data'=>$data,'kabupaten'=>$kabupaten,'pilihan'=>$data->kabupaten]);
     }
 }
