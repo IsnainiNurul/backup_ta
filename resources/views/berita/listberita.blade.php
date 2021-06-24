3355ea2a82ac0548fb840240c6412a24364bb33c
cyber12drago <naufal.refadi@yahhoo.co.id>
2021-06-24 22:24:02 +0700

    update berita

--- resources/views/berita/listberita.blade.php
+++ resources/views/berita/listberita.blade.php
@@ -347,12 +347,12 @@
                                     
                                   
                                   <div class="col-sm-12">
-                                      <div class="row">
+                                      <div class="row" style="padding-left: 10px;">
                                       @if($b->news_portal=='kompas')
-                                        <div class="col-sm-6 news-kompas news-portal">{{$b->news_portal}}</div>
+                                        <div class="col-sm-6 text-justify news-kompas news-portal">{{$b->news_portal}}</div>
 
                                       @else
-                                        <div class="col-sm-6 news-tribun news-portal">{{$b->news_portal}}</div>
+                                        <div class="col-sm-6 text-justify news-tribun news-portal">{{$b->news_portal}}</div>
                                       @endif
                                         <div class="text-left"><p style="font-size: 10px;">{{$b->date}}</p></div>
                                       </div>
