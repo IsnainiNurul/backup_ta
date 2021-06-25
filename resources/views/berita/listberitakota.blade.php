20c73d6041f8fc78db9fe2586deabb5e0ba57c18
cyber12drago <naufal.refadi@yahhoo.co.id>
2021-06-25 10:03:10 +0700

    update berita

--- resources/views/berita/listberitakota.blade.php
+++ resources/views/berita/listberitakota.blade.php
@@ -323,7 +323,7 @@
                   <div class="col-sm-6 search_bar" style="top: 60px;right: 0px;position: absolute;">
                     @if($provinsi!="" && $provinsi!="Indonesia")
                       <form method='get' action="/berita/cari">
-                        <input id="searchbar" onkeypress="search_berita()" type="text" name="cari" placeholder="Cari Berita..">
+                        <input id="searchbar" onkeypress="search_berita()" type="text" name="cari" placeholder="Cari Berisssta..">
                       </form>
                     @else
                       <form method='get' action="/berita/cari/?provinsi={{$provinsi}}">
