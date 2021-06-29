d2e7827ce68dbbce36a0270a326d6d574e77c933
cyber12drago <naufal.refadi@yahoo.co.id>
2021-06-29 15:41:11 +0700

    update berita

--- public/assets/js/berita/cari_berita.js
+++ public/assets/js/berita/cari_berita.js
@@ -3,19 +3,21 @@ function search_news() {
     input=input.toLowerCase();
     let x = document.getElementsByClassName('news-title');
     let y = document.getElementsByClassName('news-portal');
-    
+    let z = document.getElementsByClassName('news-date');
     let v = document.getElementsByClassName('div-gambar');
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             v[i].style.display="none"; 
             x[i].style.display="none";
             y[i].style.display="none";
+            z[i].style.display="none";
             
         }
         else {
             v[i].style.display="inherit"; 
             x[i].style.display="inherit";  
             y[i].style.display="inherit";
+            z[i].style.display="inherit";
                  
         }
     }
